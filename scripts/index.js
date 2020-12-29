9751bdbd59bc1f884cb87bf5d54ce76228b9a617
Ilya Avdeev <56119882+WebJSUser@users.noreply.github.com>
2020-12-29 16:57:20 +0300

    fix: Исправление Ошибок

--- scripts/index.js
+++ scripts/index.js
@@ -4,24 +4,25 @@ let popUp = document.querySelector('.popup');
 let profileTitle = document.querySelector('.profile__title');
 let profileSubTitle = document.querySelector('.profile__subtitle');
 let formElement = document.querySelector('.popup__form');
-let nameInput =  document.form.inputTitle;
-let jobInput = document.form.inputSubtitle;
+let nameInput =  formElement.inputTitle;
+let jobInput = formElement.inputSubtitle;
 
 function popUpOpenButton(){
     popUp.classList.add('popup_open');
-    popUp.classList.add('popup_overlay');
+    nameInput.value = `${profileTitle.textContent}`;
+    jobInput.value = `${profileSubTitle.textContent}`;
 }
 
 function popUpCloseButton(){
     popUp.classList.remove('popup_open');
-    popUp.classList.remove('popup_overlay');
+
 }
 
 function handleFormSubmit (evt) {
     evt.preventDefault();
+    popUpCloseButton();
     profileTitle.textContent = `${nameInput.value}`;
     profileSubTitle.textContent = `${jobInput.value}`;
-    popUpCloseButton();
 }
 
 formElement.addEventListener('submit', handleFormSubmit);
