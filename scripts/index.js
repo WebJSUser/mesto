c0f078f6e5e0245934a7a0158ab8a48541d789a7
Ilya Avdeev <56119882+WebJSUser@users.noreply.github.com>
2021-02-09 17:17:23 +0300

    End release

--- scripts/index.js
+++ scripts/index.js
@@ -37,23 +37,6 @@ const formElementEdit = popUpEditProfile.querySelector('.popup__form');
 const nameInput = formElementEdit.inputTitle;
 const jobInput = formElementEdit.inputSubtitle;
 
-formElementEdit.addEventListener('submit', (evt) => {
-    evt.preventDefault();
-    profileTitle.textContent = `${nameInput.value}`;
-    profileSubTitle.textContent = `${jobInput.value}`;
-    popUpCloseButton(popUpEditProfile)
-});
-
-popUpOpenEdit.addEventListener('click', () => {
-    nameInput.value = `${profileTitle.textContent}`;
-    jobInput.value = `${profileSubTitle.textContent}`;
-    popUpOpenButton(popUpEditProfile)
-});
-
-popUpCloseEdit.addEventListener('click', () => {
-    popUpCloseButton(popUpEditProfile)
-});
-
 // ------------------AddElement------------------
 const templateElement = document.querySelector('.element-template').content;
 const popUpAddElement = document.querySelector('.popup-add-element');
@@ -63,52 +46,9 @@ const formElementAdd = popUpAddElement.querySelector('.popup__form');
 const nameInputAdd = formElementAdd.inputTitleAdd;
 const jobInputAdd = formElementAdd.inputSubtitleAdd;
 
-formElementAdd.addEventListener('submit', (evt) => {
-    evt.preventDefault();
-    elements.prepend(addElement(
-        nameInputAdd.value,
-        jobInputAdd.value
-    ))
-    popUpCloseButton(popUpAddElement)
+// ********************* render ********************************
 
-});
-
-popUpOpenAdd.addEventListener('click', () => {
-    nameInputAdd.value = `${nameInput.textContent}`;
-    jobInputAdd.value = `${jobInput.textContent}`;
-    popUpOpenButton(popUpAddElement)
-});
-
-popUpCloseAdd.addEventListener('click', () => {
-    popUpCloseButton(popUpAddElement)
-});
-
-function addElement(name, link) {        
-    const elementItem = templateElement.querySelector('.element').cloneNode(true);
-    const titleElement = elementItem.querySelector('.element__title');
-    const imageElement = elementItem.querySelector('.element__img');
-
-    titleElement.textContent = `${name}`;
-    imageElement.src = `${link}`;
-    imageElement.alt = `Изображение ${name}`;
-
-    const btnTrash = elementItem.querySelector('.element__trash-btn');
-    btnTrash.addEventListener('click', (evt) => {
-        const trashElement = evt.target.closest('article.element')
-        trashElement.remove()
-    })
-
-    const btnLike = elementItem.querySelector('.element__like-btn');
-    btnLike.addEventListener('click', () => {
-        btnLike.classList.toggle('element__like-btn_act')
-    })
-
-    imageElement.addEventListener('click', () => popUpImageOpen(name, link))
-    
-    popUpImageClose.addEventListener('click', () => popUpCloseButton(popUpImageElement))
-
-    return elementItem;
-}
+const elements = document.querySelector('.elements');
 
 // ------------------Image------------------
 
@@ -118,25 +58,94 @@ const popUpImageCap = popUpImageElement.querySelector('.popup__caption');
 const popUpImageClose = popUpImageElement.querySelector('.popup__close-btn');
 
 const popUpImageOpen = (name, link) => {
-    popUpImage.src = `${link}`;
+    popUpImage.src = link;
     popUpImage.alt = `Изображение ${name}`; 
-    popUpImageCap.textContent = `${name}`;    
-    popUpOpenButton(popUpImageElement)
-  }      
+    popUpImageCap.textContent = name;    
+    openPopup(popUpImageElement)
+  }    
 
 // *********************Close/Open********************************
 
-function popUpOpenButton(arg) {
+function openPopup(arg) {
     arg.classList.add('popup_open');
 }
 
-function popUpCloseButton(arg) {
+function closePopup(arg) {
     arg.classList.remove('popup_open');
 }
 
+
+// ------------------AddElement------------------
+
+function addElement(name, link) {        
+    const elementItem = templateElement.querySelector('.element').cloneNode(true);
+    const titleElement = elementItem.querySelector('.element__title');
+    const imageElement = elementItem.querySelector('.element__img');
+
+    titleElement.textContent = name;
+    imageElement.src = link;
+    imageElement.alt = `Изображение ${name}`;
+
+    const btnTrash = elementItem.querySelector('.element__trash-btn');
+    btnTrash.addEventListener('click', (evt) => {
+        evt.target.closest('article.element').remove();
+    })
+
+    const btnLike = elementItem.querySelector('.element__like-btn');
+    btnLike.addEventListener('click', () => {
+        btnLike.classList.toggle('element__like-btn_act')
+    })
+
+    imageElement.addEventListener('click', () => popUpImageOpen(name, link))
+    
+    popUpImageClose.addEventListener('click', () => closePopup(popUpImageElement))
+
+    return elementItem;
+}
+
+// ------------------EditProfile------------------
+
+formElementEdit.addEventListener('submit', (evt) => {
+    evt.preventDefault();
+    profileTitle.textContent = nameInput.value;
+    profileSubTitle.textContent = jobInput.value;
+    closePopup(popUpEditProfile)
+});
+
+popUpOpenEdit.addEventListener('click', () => {
+    nameInput.value = profileTitle.textContent;
+    jobInput.value = profileSubTitle.textContent;
+    openPopup(popUpEditProfile)
+});
+
+popUpCloseEdit.addEventListener('click', () => {
+    closePopup(popUpEditProfile)
+});
+
+// ------------------AddElement------------------
+
+formElementAdd.addEventListener('submit', (evt) => {
+    evt.preventDefault();
+    elements.prepend(addElement(
+        nameInputAdd.value,
+        jobInputAdd.value
+    ))
+    closePopup(popUpAddElement)
+
+});
+
+popUpOpenAdd.addEventListener('click', () => {
+    nameInputAdd.value = nameInput.textContent;
+    jobInputAdd.value = jobInput.textContent;
+    openPopup(popUpAddElement)
+});
+
+popUpCloseAdd.addEventListener('click', () => {
+    closePopup(popUpAddElement)
+});
+
 // ********************* render ********************************
 
-const elements = document.querySelector('.elements');
 function render(){      
     initialCards.forEach((arg) => {
         elements.append(addElement(arg.name, arg.link));
